c42893ba2403620a00e3919bafeecaf603e5e457
Иван <kleimenov416@gmail.com>
2020-09-03 21:02:02 -0400

    JS code in slider is changed

--- index.js
+++ index.js
@@ -86,16 +86,15 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    console.log('Hey');
-    var i;
-    var slides = document.getElementsByClassName("sliderImg");
-    var dots = document.getElementsByClassName("dot");
+   
+    let slides = document.getElementsByClassName("sliderImg");
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1} ;
     if (n < 1) {slideIndex = slides.length};
-    for (i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; 
     };
-    for (i = 0; i < dots.length; i++) {
+    for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     };
     slides[slideIndex-1].style.display = "block"; 
@@ -107,9 +106,9 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
+  
   var slides = document.getElementsByClassName("sliderImg");
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"; 
   }
   slideIndex++;
