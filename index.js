d9a66207101257b011c7941ba423f2f9ad347a0d
Иван <kleimenov416@gmail.com>
2020-09-08 12:02:21 -0400

    add weather widget

--- index.js
+++ index.js
@@ -142,3 +142,120 @@ function showSlides() {
    
 */
    
+
+/*-------*/
+
+const notificationElement = document.querySelector('.notification');
+const iconElement = document.querySelector('.weather-icon');
+const tempElement =document.querySelector('.temperature-value p');
+const descElement = document.querySelector('.temperature-description p');
+const locationElement = document.querySelector('.location p');
+
+//to display the weather
+function displayWeather() {
+    iconElement.innerHTML = `<img src="assets/${weather.iconId}.png"/>`;
+
+    tempElement.innerHTML = `${weather.temperature.value}° <span>C</span>`;
+
+    descElement.innerHTML = weather.description; 
+
+
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+
+}
+
+function celsiusToFahrenheit(temperature) {
+    return (temperature * 9/5) + 32;
+}
+
+function fahrenheitToCelsius(temperature) {
+    return 9/5 * (temperature - 32);
+}
+
+tempElement.addEventListener('click', function() {
+
+    if(weather.temperature.value==="undefined") {return};
+
+    if(weather.temperature.unit === 'celsius') {
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}° <span>F</span>`;
+
+        weather.temperature.unit ="fahrenheit";
+    }
+    else {
+        /*
+        let celsius = fahrenheitToCelsius(weather.temperature.value);
+        celsius = Math.floor(celsius);
+        */
+       tempElement.innerHTML = `${weather.temperature.value}° <span>C</span>`;
+        
+       weather.temperature.unit ="celsius";
+    }
+}
+
+)
+
+/*
+if("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}
+else {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p> Browser Doesn't support Geolocation.</p>"
+}
+
+ 
+function setPosition(position) {
+    console.log("setPosition ");
+    let latitude = position.coords.latitude;
+    let longtitude = position.coords.longtitude;
+    
+    getWeather(latitude, longtitude)
+}
+
+function showError(error) {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+*/
+getWeather()
+const weather ={};
+weather.temperature = {
+    unit: "celsius"
+}
+
+// app consts and var
+const KELVIN = 273;
+
+//API key
+
+const key = '072e3ec2199386d51ce3c8b4fd8b194d';
+
+//src
+const linkApi = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY';
+
+function getWeather() {
+    console.log("first");
+    // let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=Reykjavik&appid=072e3ec2199386d51ce3c8b4fd8b194d`;
+
+    fetch(api).then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        console.log("second");
+        weather.temperature.value=Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].description;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+    })
+    .then(function(){
+        console.log("third");
+        displayWeather();
+    });
+}
